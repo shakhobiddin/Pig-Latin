a52ec0dbdd31e84ecb4b952219657fdd410c2388
shakhobiddin <36891629+shakhobiddin@users.noreply.github.com>
2018-04-25 14:44:08 +0200

    Update question 4.py

--- question 4.py
+++ question 4.py
@@ -4,11 +4,9 @@ sentence= input("Enter a word:")
 if len(sentence) > 0 and sentence.isalpha():
     word = sentence.lower()
     a = word[0]
-    if a == ('a' or 'e' or 'i' or 'o' or 'u'):
-        new_word = word + pig
-        print (new_word)
-    else:
-        new_word = word[1:] + a + pig
-        print (new_word)
+    new_word = word + pig
+
+    new_word = word[1:] + a + pig
+    print (new_word)
 else:
     print ('empty')
