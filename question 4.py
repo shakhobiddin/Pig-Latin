6abddf642d0c64bd8e55de8a2989b72289bfbec2
shakhobiddin <36891629+shakhobiddin@users.noreply.github.com>
2018-04-25 11:18:55 +0200

    Update question 4.py

--- question 4.py
+++ question 4.py
@@ -4,7 +4,7 @@ sentence= input("Enter a word:")
 if len(sentence) > 0 and sentence.isalpha():
     a = sentence.lower()
     a = word[0]
-    if first == ('a' or 'e' or 'i' or 'o' or 'u'):
+    if a == ('a' or 'e' or 'i' or 'o' or 'u'):
         new_word = word + pig
         print (new_word)
     else:
